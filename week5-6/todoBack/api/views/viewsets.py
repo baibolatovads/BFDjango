9fa11a7a095956160353aaa4fcc86e8c0bbdac1b
Dariya Baibolatova <baibolatovads@gmail.com>
2020-04-19 04:38:54 +0600

    week 13-14 changes

--- week5-6/todoBack/api/views/viewsets.py
+++ week5-6/todoBack/api/views/viewsets.py
@@ -35,24 +35,18 @@ class TaskViewSet(mixins.CreateModelMixin,
 
     def perform_create(self, serializer):
         serializer.save()
-        if self.action == 'create':
-            logger.debug('Task is created, ID: {}'.format(serializer.instance))
-            logger.info('Task is created, ID: {}'.format(serializer.instance))
-            logger.warning('Task is created, ID: {}'.format(serializer.instance))
-            logger.error('Task is created, ID: {}'.format(serializer.instance))
-            logger.critical('Task is created, ID: {}'.format(serializer.instance))
-        if self.action == 'update':
-            logger.debug('Task is updated, ID: {}'.format(serializer.instance))
-            logger.info('Task is updated, ID: {}'.format(serializer.instance))
-            logger.warning('Task is updated, ID: {}'.format(serializer.instance))
-            logger.error('Task is updated, ID: {}'.format(serializer.instance))
-            logger.critical('Task is updated, ID: {}'.format(serializer.instance))
-        if self.action == 'destroy':
-            logger.debug('Task is deleted, ID: {}'.format(serializer.instance))
-            logger.info('Task is deleted, ID: {}'.format(serializer.instance))
-            logger.warning('Task is deleted, ID: {}'.format(serializer.instance))
-            logger.error('Task is deleted, ID: {}'.format(serializer.instance))
-            logger.critical('Task is deleted, ID: {}'.format(serializer.instance))
+        logger.debug('Task is created, ID: {}'.format(serializer.instance))
+        logger.info('Task is created, ID: {}'.format(serializer.instance))
+
+    def perform_update(self, serializer):
+        logger.debug('Task is updated, ID: {}'.format(serializer.instance))
+        logger.info('Task is updated, ID: {}'.format(serializer.instance))
+        logger.warning('Task is updated, ID: {}'.format(serializer.instance))
+
+    def perform_destroy(self, instance):
+        logger.warning('Task is deleted, ID: {}'.format(instance))
+
+
 
 class TaskListViewSet(mixins.CreateModelMixin,
                       mixins.ListModelMixin,
@@ -72,9 +66,12 @@ class TaskListViewSet(mixins.CreateModelMixin,
         serializer.save()
         logger.debug('Task List is created: {}'.format(serializer.instance))
         logger.info('Task List is created: {}'.format(serializer.instance))
-        logger.warning('Task List is created: {}'.format(serializer.instance))
-        logger.error('Task List is created: {}'.format(serializer.instance))
-        logger.critical('Task List is created: {}'.format(serializer.instance))
 
+    def perform_update(self, serializer):
+        logger.debug('Task List is updated, ID: {}'.format(serializer.instance))
+        logger.info('Task List is updated, ID: {}'.format(serializer.instance))
+        logger.warning('Task List is updated, ID: {}'.format(serializer.instance))
 
+    def perform_destroy(self, instance):
+        logger.warning('Task List is deleted, ID: {}'.format(instance))
 
